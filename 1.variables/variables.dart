bf0d993479ebc7b30e9b03e4947ce45049dee7b2
jaev-977 <john-977@hotmail.com>
2021-09-04 08:17:49 -0500

    variables - null- final-const

---
+++ 1.variables/variables.dart
@@ -0,0 +1,20 @@
+void main() {
+  var nombre = "Juan";
+  String nombre_dos = "Juan";
+  var edad = 89;
+  int edad_dos = 28;
+  var test;
+  test = 100;
+
+  //null safety
+  int? variableNull = null;
+  String? variableStringNull = null;
+
+  //final - constante
+  final String variableFinalTest = "Hola";
+  const String variableConstanteTest = "Hola";
+
+  print(nombre_dos.toUpperCase());
+  print(nombre.toUpperCase());
+  print(test);
+}
