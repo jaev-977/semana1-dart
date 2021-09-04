48a276be40ff31b11759c639a8362b854bf6b9d7
jaev-977 <john-977@hotmail.com>
2021-09-04 10:55:27 -0500

    Operadores ejercicios

---
+++ 4.operadores/operadores.dart
@@ -0,0 +1,10 @@
+void main() {
+  var variable1 = 5;
+  var variable2 = 4.0;
+
+  var variable3 = variable1 > 5 ? 'si' : 'no';
+  var variable4;
+  var variable5 = variable4 ?? 'variable 5 toma este valor';
+
+  print(variable5);
+}
